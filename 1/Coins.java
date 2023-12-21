7f18c9f2e8558628c40c59fdc0b11534ed2a8995
chenhadad2 <chen.hadad@post.runi.ac.il>
2023-12-21 16:24:56 +0200

    2112 16:24

---
+++ 1/Coins.java
@@ -0,0 +1,10 @@
+class Coins {
+public static void main(String args[]) {
+	int a = Integer.parseInt(args[0]);
+	
+	int x = a/25;
+	int y = a%25;
+	
+System.out.println("Use" + " " + x + " " + "quarters" + " " + "and" + " " + y + " " + "cents");
+}
+}
---
+++ 1/Gen3.java
@@ -0,0 +1,29 @@
+class Gen3 {
+public static void main(String args[]) {
+        // prints a random value in [a,b)
+		
+	int a = Integer.parseInt(args[0]);
+	int b = Integer.parseInt(args[1]);
+	
+	int range = ((b - a) + 1);
+
+		
+        int x = (int)(Math.random()* range + a);
+        System.out.println(x);
+
+       int y = (int)(Math.random()* range + a);
+        System.out.println(y);
+		
+		int z = (int)(Math.random()* range + a);
+        System.out.println(z);
+		
+		int t = (int)(Math.min(x,y));
+		int l = (int)(Math.min(x,z));
+		int f =  (int)(Math.min(l,t));
+        System.out.println(f);
+		
+	
+
+		
+    }
+}
