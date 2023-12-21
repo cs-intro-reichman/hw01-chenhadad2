d28e9534e44ff9e5b4645c45cbaf8f1fab13ba46
chenhadad2 <chen.hadad@post.runi.ac.il>
2023-12-21 18:27:07 +0200

    18:27

--- GenThree.java
+++ GenThree.java
@@ -4,15 +4,14 @@ public static void main(String args[]) {
 		
 	int a = Integer.parseInt(args[0]);
 	int b = Integer.parseInt(args[1]);
-	int range = ((b - a) + 1);
     
-	int x = (int)(Math.random()* range + a);
+	int x = (int)((Math.random()* ( b - a + 1)) + a);
         System.out.println(x);
 
-	int y = (int)(Math.random()* range + a);
+	int y = (int)((Math.random()* ( b - a + 1)) + a);
         System.out.println(y);
 		
-	int z = (int)(Math.random()* range + a);
+	int z = (int)((Math.random()* ( b - a + 1)) + a);
         System.out.println(z);
 		
 	int t = (int)(Math.min(x,y));
