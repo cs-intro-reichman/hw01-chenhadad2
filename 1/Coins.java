d7d6c231ee41e45033bc571d919b693fabec8d02
chenhadad2 <chen.hadad@post.runi.ac.il>
2023-12-21 16:30:46 +0200

    21.12

--- 1/Coins.java
+++ 1/Coins.java
@@ -1,9 +1,8 @@
 class Coins {
 public static void main(String args[]) {
 	int a = Integer.parseInt(args[0]);
-	
-	int x = a/25;
-	int y = a%25;
+	int x = a / 25;
+	int y = a % 25;
 	
 System.out.println("Use" + " " + x + " " + "quarters" + " " + "and" + " " + y + " " + "cents");
 }
